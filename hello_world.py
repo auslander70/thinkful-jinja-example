6b8f6261230db684adbda8806db934545642bba1
Wil DAmato <wil.damato@gmail.com>
2015-12-28 20:36:41 +0000

    hello world via jinja complete

--- hello_world.py
+++ hello_world.py
@@ -3,10 +3,37 @@ from os import environ
 
 app = Flask(__name__)
 
+class jediname(object):
+  def __init__(self, firstname, lastname):
+    self.firstname = firstname
+    self.lastname = lastname
+    
+  def JediNameConstructor(self):
+    part1 = self.firstname[:2]
+    part2 = self.lastname[:3]
+    name = part2 + part1
+    return name
+    
+    
 @app.route("/")
 @app.route("/hello")
 def hello_world():
-  return render_template('hello_world.html')
+  return render_template('hello.html',
+                          my_name='world')
+
+@app.route("/hello/<name>")
+def hello_name(name):
+  return render_template('hello.html',
+                          my_name=name)
+                          
+@app.route("/jedi/<firstname>/<lastname>")
+def jedi(firstname,lastname):
+  myJedi = jediname(firstname, lastname)
+  myJediName = myJedi.JediNameConstructor()
+  return render_template('hello.html',
+                          my_name=myJediName)
+
+
   
 if __name__ == '__main__':
   app.run(debug=True, host="0.0.0.0", port=8080)
